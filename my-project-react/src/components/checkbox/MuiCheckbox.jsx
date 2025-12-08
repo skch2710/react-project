f23e874f329656582a7cce30f24b0188194d4ab2
sathish kumar <skch2710@gmail.com>
2025-12-08 15:47:04 +0530

    delete duplicate

--- my-project-react/src/components/checkbox/MuiCheckbox.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Checkbox } from "@mui/material";
-
-const MuiCheckbox = (props) => {
-  return <Checkbox size="small" {...props} />;
-};
-
-export default MuiCheckbox;
